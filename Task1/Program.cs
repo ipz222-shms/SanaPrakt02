fce586121ef1775cbc69af630aa29fa00c2a1b5b
Микита Шевцов <ipz222_shms@student.ztu.edu.ua>
2022-11-06 13:51:08 +0200

    Changed 3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("Практична робота №2");
+Console.WriteLine("Шевцов М.С.");
+Console.WriteLine("Hello World!");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
